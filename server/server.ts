54425f26c58fc8de12485c7dce354ad15e6ea0f1
Andres Alvarado <aalvarado@atlassian.com>
2023-06-07 15:15:37 -0500

    editable to do list

--- server/server.ts
+++ server/server.ts
@@ -15,13 +15,40 @@ const messages: any[] = [];
 const getMessages = (request: any, response: any) => { response.json(messages); }
 
 const postMessages = (request: any, response: any) => {
+    const action = request.query.action;
+
+    if (action === 'deleteAll') {
+        messages.length = 0;
+        response.status(200).json({ message: 'All messages deleted successfully' });
+      } else {
+    
     console.log('start')
     messages.push(request.body)
     response.status(201)
     response.json(request.body)
     console.log('end')
-    return
+    return}
 }
+
+const deleteMessage = (request: any, response:any) => {
+    const messageId = request.params.id;
+    const messageIndex = messages.findIndex((message) => message.id === messageId);
+    messages.splice(messageIndex, 1);
+    response.status(200).json({ message: 'Message deleted successfully' });
+} ;
+
+const deleteAll= (request:any ,response:any) => {
+    messages.splice(0,messages.length);
+    response.status(200).json({ message: 'Messages deleted successfully' });
+}
+
+const updateMessage= (request: any, response:any) => {
+    const messageId = request.params.id;
+    const messageIndex = messages.findIndex((message) => message.id === messageId);
+    messages.splice(messageIndex, 1, request.body);
+    response.status(200).json({ message: 'Message deleted successfully' });
+
+};
 
 // user
 app.get('/api/messages', getMessages);
@@ -29,6 +56,11 @@ app.get('/api/messages', getMessages);
 // messages
 app.post('/api/messages', postMessages);
 
+app.delete('/api/messages/:id', deleteMessage);
+
+app.delete('/api/messages', deleteAll);
+
+app.put('/api/messages/:id', updateMessage);
 
 // tslint:disable-next-line:no-console
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import { v4 as uuidv4 } from 'uuid';
 
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function App() {
-  const [messages, setMessages] = React.useState<{message: string}[]>();
+  const [messages, setMessages] = React.useState<{message: string, id : number}[]>([]);
+  const [input, setInput] = React.useState('');
+  
 
 React.useEffect(() => {
   fetchMessages()
@@ -12,14 +19,202 @@ React.useEffect(() => {
 
 const fetchMessages = async () => {
   const response = await fetch('http://localhost:3000/api/messages')
-  setMessages(await response.json())
+  const responseBody=await response.json();
+  setMessages(responseBody);
+  
 }
+
+const deleteMessages = async () => {
+  await fetch(`http://localhost:3000/api/messages`, {
+    method: 'DELETE',headers: {
+      "Content-Type": "application/json",
+    }
+  });
+  fetchMessages();
+  
+}; 
+
+const deleteMessage = async (id:any) => {
+  await fetch(`http://localhost:3000/api/messages/${id}`, {
+    method: 'DELETE', headers: {
+      "Content-Type": "application/json",
+    }
+  });
+  fetchMessages();
+  
+}; 
+
+const editMessage = async (id:any, newMessage:any) => {
+  const updatedMessage= {
+    ...messages.find((msg) => msg.id === id),
+    message: newMessage,};
+  await fetch(`http://localhost:3000/api/messages/${id}`, {
+    method: 'PUT', body: JSON.stringify(updatedMessage) , headers: {
+      "Content-Type": "application/json",
+    }
+  });
+  fetchMessages();
+}
+
+const handleClick = async() => {
+  await createMessage();
+  
+  
+  setInput('');
+
+};
+
+
+
+const createMessage = async () => {
+  const newMessage = { message: input, id: uuidv4()};
+ 
+  await fetch(`http://localhost:3000/api/messages`, {
+    method: 'POST', body: JSON.stringify(newMessage), headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  fetchMessages();
+  
+};
+
+
+const handleChange = (e :any) => {setInput(e.target.value)}
+
+const DraggableMessage = ({ message, index, moveMessage, editMessage }:any) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'message',
+    item: { id: message.id, index },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [editedMessage, setEditedMessage] = React.useState(message.message);
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: 'message',
+    drop: (item:any) => moveMessage(item.index, index),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleTextFieldChange = (e:any) => {
+    setEditedMessage(e.target.value);
+  };
+
+  const handleEditSave = () => {
+    editMessage(message.id, editedMessage);
+    setIsEditing(false);
+  };
+
   return (
-    <div className="App">
-      {messages?.map(message => <div>{message.message}</div>)}
-    
+    <div
+      ref={(node) => drag(drop(node))}
+      key={message.id}
+      style={{
+        backgroundColor: isDragging || isOver ? '#f0f0f0' : '#ffffff',
+        borderColor: '#ffffff',
+        borderStyle: 'solid',
+        borderWidth: '1px',
+        padding: '10px',
+        margin: '5px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '200px',
+        textAlign: 'left',
+        fontSize: '16px',
+        color: 'black'
+      }}
+    >
+      {isEditing ? (
+      <>
+        <TextField value={editedMessage}
+          onChange={handleTextFieldChange}
+          autoFocus
+          onBlur={handleEditSave}
+        />
+      </>
+    ) : (
+      <span onClick={handleEditClick}>
+      {message.message}
+      </span> )}
+      <button
+        onClick={() => deleteMessage(message.id)}
+        style={{
+          backgroundColor: 'transparent',
+          border: 'none',
+          color: 'inherit',
+          cursor: 'pointer',
+          fontSize: '14px',
+          outline: 'none',
+        }}
+      >
+        {'\u00D7'}
+      </button>
     </div>
   );
+};
+
+
+const moveMessage = (dragIndex:any, hoverIndex:any) => {
+  const draggedMessage = messages[dragIndex];
+  const newMessages = [...messages];
+  newMessages.splice(dragIndex, 1);
+  newMessages.splice(hoverIndex, 0, draggedMessage);
+  setMessages(newMessages);
+};
+
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+    <div className="App" 
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundColor: '#282c34',
+        color: 'white'
+        
+      }}>
+      <h1 style={{ marginBottom: '20px' }}>The Great To Do List</h1>
+      <TextField id="Enter message" label="Add Item To Do" variant="outlined"  style ={{marginBottom: '10px'}} value={input} onChange ={handleChange}
+          sx={{
+            '& label': { color: '#4E8282' }, 
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': { borderColor: '#4E8282' }, 
+              '&:hover fieldset': { borderColor: '#4E8282' }, // Change outline color on hover
+            },
+            '& input': { color: '#4E8282' }, 
+      }}/>
+      <div>
+        <Button variant="contained" style ={{marginBottom: '10px',    width: '80px',height: '30px',marginLeft: '4px',}} onClick = {deleteMessages}>Clear</Button>
+        <Button variant="contained" style ={{marginBottom: '10px' , marginLeft: '4px',    width: '80px', height: '30px'}} onClick = {handleClick}>Submit </Button>
+      </div>
+      {messages?.map((message,index) => (
+        
+        <DraggableMessage
+            key={message.id}
+            message={message}
+            index={index}
+            moveMessage={moveMessage}
+            editMessage={editMessage}
+            />
+
+      ))}
+    
+    </div>
+    </DndProvider>
+  );
+  
 }
 
 export default App;
